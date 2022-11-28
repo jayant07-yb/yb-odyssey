c6aa937ab504fe569f9c1853f9d92ffaeb3b48ae
janand <janand@yugabyte.com>
2022-11-28 08:59:31 +0000

    Added the changes for using same pool for multiple roles

--- sources/deploy.c
+++ sources/deploy.c
@@ -28,7 +28,7 @@ int od_deploy(od_client_t *client, char *context)
 	query_size = kiwi_vars_cas(&client->vars, &server->vars, query,
 				   sizeof(query) - 1);
 
-	if (query_size > 0) {
+	if  (0) {
 		query[query_size] = 0;
 		query_size++;
 		machine_msg_t *msg;
@@ -49,6 +49,33 @@ int od_deploy(od_client_t *client, char *context)
 	} else {
 		client->server->synced_settings = true;
 	}
+
+	/* Changes for the SET ROLE */
+	{
+	char set_role_query[OD_QRY_MAX_SZ];
+	sprintf(set_role_query,"SET role = %s ;",client->startup.user.value);
+	int set_role_len = strlen(set_role_query);
+	set_role_query[set_role_len] = 0 ;
+	set_role_len++;
+
+	machine_msg_t *msg;
+	msg = kiwi_fe_write_query(NULL, set_role_query, set_role_len);
+	if (msg == NULL)
+		return -1;
+
+	int rc;
+	rc = od_write(&server->io, msg);
+	if (rc == -1)
+		return -1;
+	
+	client->server->synced_settings = false;
+
+	od_debug(&instance->logger, context, client, server,
+			 "deploy: %s", set_role_query);
+	query_count++;
+	}
+
+
 
 	return query_count;
 }
